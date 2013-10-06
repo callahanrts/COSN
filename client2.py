88cecb893dd0065335efd04a9956ea6649ad83b1
Cody Callahan <CallahanRTS@gmail.com>
2013-10-06 14:43:53 -0700

    initial commit

---
+++ client2.py
@@ -0,0 +1,42 @@
+from array import *
+from socket import *
+import pickle
+
+client_socket = socket(AF_INET, SOCK_DGRAM)  #This creates socket
+client_socket.bind(('', 9002))
+
+user = "2" 
+
+# Commands
+register = ["REGISTER", user, "", "9002"] # Response => ACK
+query    = ["QUERY", "1"]                # Response => LOCATION
+logout   = ["LOGOUT", user]
+
+while True:
+  command = input("Enter a command: ").upper()
+  if command == "REGISTER":
+    client_socket.sendto(pickle.dumps(register), ("localhost",9000))
+
+  elif command == "QUERY":
+    query[1] = str(input("Enter a user id: "))
+    client_socket.sendto(pickle.dumps(query), ("localhost",9000))
+
+  elif command == "LIST":
+    print("REGISTER")
+    print("QUERY")
+    print("LOGOUT")
+
+  elif command == "LOGOUT":
+    client_socket.sendto(pickle.dumps(logout), ("localhost",9000))
+
+  else:
+    print("ERROR: command not recognized")
+    continue
+
+  print("Sending request")
+  recv_data, addr = client_socket.recvfrom(1024)
+  data = pickle.loads(recv_data) 
+  print(data)
+
+client_socket.close()
+
---
+++ server.py
@@ -0,0 +1,63 @@
+from socket import *
+import time
+import sqlite3
+import pickle
+
+
+# Connect to sqlite database and print success message
+conn = sqlite3.connect('cosn.db')
+print("Opened database successfully")
+
+# Delete records of online users
+conn.execute("DELETE FROM online_users")
+conn.commit()
+
+#Connect to server and print message 
+server_socket = socket(AF_INET, SOCK_DGRAM)  
+server_socket.bind(('', 9000))
+print("(9000) UDP Server Waiting for client...")
+
+# Wait for connections from clients
+while True:
+  # Retrieve data from request
+  data, address = server_socket.recvfrom(1024)
+  request = pickle.loads(data)
+  command = request[0]
+
+  # Switch on commands
+  if command == "REGISTER":
+    cursor = conn.execute("SELECT * FROM online_users WHERE id = ? LIMIT 1", str(request[1]))
+    exists = False
+    for row in cursor:
+      if row[0]:
+        exists = True 
+    if exists:
+      print("User is already online")
+      server_socket.sendto(pickle.dumps(["ACK", row[0], row[1], row[2]]), address)
+    else:
+      conn.execute("INSERT INTO online_users(id, ip_address, port) VALUES(?, ?, ?)", (request[1], request[2], request[3]))
+      conn.commit()
+      print("User has been registered")
+      server_socket.sendto(pickle.dumps(["ACK", request[1], request[2], request[3]]), address)
+  
+  elif command == "QUERY":
+    cursor = conn.execute("SELECT * FROM online_users WHERE id = ? LIMIT 1", str(request[1]))
+    exists = False
+    for row in cursor:
+      if row[0]:
+        exists = True 
+    if exists:
+      print("Query for user " + row[1])
+      server_socket.sendto(pickle.dumps(["LOCATION", row[0], row[1], row[2]]), address)
+    else:
+      print("User was not found")
+      server_socket.sendto(pickle.dumps(["Error", "User was not found"]), address)
+
+  elif command == "LOGOUT":
+    conn.execute("DELETE FROM online_users WHERE id = ?", request[1])
+    server_socket.sendto(pickle.dumps(["User was logged out successully"]), address)
+    conn.commit()
+    print("User logged out")
+
+  else:
+    print("Invalid data from client ( " ,address[0], " " , address[1] , " ): ", command)
