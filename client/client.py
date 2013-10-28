f6fb82707c41a0147345d61261b2bda61622d065
Cody Callahan <CallahanRTS@gmail.com>
2013-10-27 17:05:48 -0700

    finished refactoring. could probably use a couple more classes somewhere though

--- client/client.py
+++ client/client.py
@@ -69,7 +69,7 @@ class Client:
       # Generate template user profile or blank
       copyfile("../extras/profile.json", "../users/" + self.username + "/" + self.username + ".json")
       self.user = open(self.user_directory + self.username + ".json")
-      
+
   def create_dir_if_not_exists(self, path):
     if not os.path.exists(path): os.makedirs(path)
 
@@ -89,12 +89,15 @@ class Client:
     # UDP Socket to connect with server
     udp_socket = socket(AF_INET, SOCK_DGRAM)
     udp_socket.settimeout(2)
-    print(send_message)
+
     try: 
+      # Try to send data over UDP
       udp_socket.sendto(pickle.dumps(send_message), self.server_addr)
       recv_data, addr = udp_socket.recvfrom(1024)
       return pickle.loads(recv_data) 
+
     except timeout:
+      # Catch server timeout error
       self.view.log("Server timed out")
 
 
@@ -105,8 +108,7 @@ class Client:
     # Create socket to connect with a user
     self.chat_conn = socket(AF_INET, SOCK_STREAM)
 
-    user_data = self.send_udp(self.servecmd.query_user(user))
-
+    # Respond to commands
     if command == "FRIEND":
       self.friends.append(user)
       send_message = self.clientcmd.befriend_user(self.username)
@@ -123,8 +125,9 @@ class Client:
       send_message = self.clientcmd.request_file(alt_data)
       request_for_file = True
 
+    # Get user data
+    user_data = self.send_udp(self.servecmd.query_user(user))
     try:
-      print(user_data)
       self.chat_conn.connect((user_data[2], int(user_data[3])))
       self.chat_conn.send(pickle.dumps(send_message)) 
       recv_data, addr = self.chat_conn.recvfrom(1024)
@@ -132,52 +135,61 @@ class Client:
 
       if request_for_profile:
         self.save_profile(response)
+
       elif request_for_file:
         self.save_file(response)
+
       else:
         self.view.log(response) 
-    except:
+    except: # log errors
       logging.exception("hm")
       self.view.log(self.send_udp(self.clientcmd.user_offline(user_data[4])))
 
     self.chat_conn.close()
 
   def save_file(self, data):
-    recv_data, addr = self.chat_conn.recvfrom(data + 1024)
-    resp_file = pickle.loads(recv_data) 
+    resp_file = self.retrieve_data()
 
+    # Set directory or create if needed
     directory = "../users/"+self.username+"/friends/files/"
-    if not os.path.exists(directory):
-      os.makedirs(directory)
+    self.create_dir_if_not_exists(directory)
 
-    if data > 0:
-      # create file
+    if data > 0:  # Create File
       f = open(directory+resp_file[1], "wb")
       f.write(resp_file[3])
       f.close()
     else: 
       self.view.log(resp_file)
 
-  def save_profile(self, data):
+  def retrieve_data(self):
     recv_data, addr = self.chat_conn.recvfrom(data + 1024)
-    resp_file = pickle.loads(recv_data)
-    if data > 0:
-      directory = "../users/"+self.username+"/friends/"+resp_file[1]+"/"
-      json_profile = json.loads(resp_file[3])
-      if not os.path.exists(directory):
-        os.makedirs(directory)
+    return pickle.loads(recv_data) 
 
+  def save_profile(self, data):
+    resp_file = retrieve_data()
+    if data > 0:
+      # Set directory or create if needed
+      directory = "../users/"+self.username+"/friends/"+resp_file[1]+"/"
+      self.create_dir_if_not_exists(directory)
+
+      # Parse JSON profile
+      json_profile = json.loads(resp_file[3])
+
+      # Write to file
       with open(directory + resp_file[1] + ".json", "w") as outfile:
         json.dump(json_profile, outfile, indent=2)
+    
     else:
       self.view.log(resp_file)
 
   # Current, single window chat
   def chat_command(self, message, user): 
     if user in self.friends:
+      # Open connection and get user
       self.chat_conn = socket(AF_INET, SOCK_STREAM)
       user_data = self.send_udp(self.servecmd.query_user(user))
       self.view.log_message(self.username+": "+message)
+
       try:
         self.chat_conn.connect((user_data[2], int(user_data[3])))
         self.chat_conn.send(pickle.dumps(self.clientcmd.chat_message(message, self.username, self.chat_counter))) 
@@ -198,6 +210,9 @@ class Client:
     else:
       self.view.log("You must be friends with this user before chatting with them")
 
+  def accept_connection(self, s):
+
+
   def peer_listener(self):
     # Set up main, non-blocking, server socket to listen for tcp connections
     tcp_socket = socket(AF_INET, SOCK_STREAM)
@@ -205,77 +220,58 @@ class Client:
     tcp_socket.bind((self.host, self.port))
     tcp_socket.listen(1024)
 
-    # Sockets from which we expect to read
-    inputs = [ tcp_socket ]
-
-    # Sockets to which we expect to write
-    outputs = [ ]
-
-    # Outgoing message queues (socket:Queue)
-    message_queues = {}
+    inputs = [ tcp_socket ]  # Sockets from which we expect to read
+    outputs = [ ]            # Sockets to which we expect to write
+    message_queues = {}      # Outgoing message queues (socket:Queue)
 
     while inputs:
-      print("===========================================================================")
       # Wait for at least one of the sockets to be ready for processing
       readable, writable, exceptional = select.select(inputs, outputs, inputs)
 
       # Handle inputs
       for s in readable:
         if s is tcp_socket:
-          connection, client_address = s.accept()
-          connection.setblocking(0)
-          inputs.append(connection)
-
-          # Give the connection a queue for data we want to send
-          message_queues[connection] = queue.Queue()
-          print("Inputs:")
-          print(inputs)
+          connection, client_address = s.accept()    # Accept connection
+          connection.setblocking(0)                  # Don't allow socket blocking
+          inputs.append(connection)                  # add connection to inputs arr
+          message_queues[connection] = queue.Queue() # Create message queue
 
         else:
           data = s.recv(1024)
-          if data: # A readable client socket has data
-            # Parse message using pickle
-            message = pickle.loads(data)
-            self.view.log(message)
+          if data: 
+            message = pickle.loads(data) # Parse message using pickle
+            self.view.log(message)       # Log message to gui
   
-            # Print to console for debugging
-            print("Readable client socket has data")
-            print(message)
-
             # Switch on the different types of messages or return original if not recognized
             if message[0] == "FRIEND":
-              self.friends.append(message[1])
-              message_queues[s].put(pickle.dumps(self.cmd.confirm))
+              self.friends.append(message[1])                       # Add to friends list
+              message_queues[s].put(pickle.dumps(self.cmd.confirm)) # Queue message to be returned
   
             elif message[0] == "CHAT": 
               chat_message = message[3]
               self.view.username.set(message[2])            
               self.view.log_message(message[2]+": "+message[1])
   
-              # Send Size of the message
-              message_queues[s].put(pickle.dumps(sys.getsizeof(chat_message)))
-
-              # Send message
-              message_queues[s].put(pickle.dumps(self.clientcmd.delivered_message(chat_message)))
+              message_queues[s].put(pickle.dumps(sys.getsizeof(chat_message)))                    # Send Size of the message
+              message_queues[s].put(pickle.dumps(self.clientcmd.delivered_message(chat_message))) # Send message
 
             elif message[0] == "REQUEST":
               size = os.path.getsize(self.user_directory + self.username + ".json")
-              send_message = self.clientcmd.profile_message(message[1], message[2], json.dumps(self.user))
-              message_queues[s].put(pickle.dumps(size))
-              message_queues[s].put(pickle.dumps(send_message))
-
+              send_message = self.clientcmd.profile_message(message[1], message[2], json.dumps(self.user)) 
+              message_queues[s].put(pickle.dumps(size))          # Send size of the message
+              message_queues[s].put(pickle.dumps(send_message))  # Send message
+ 
             elif message[0] == "RELAY": 
               profile_file = self.friends_directory + message[1] + "/" + message[1] + ".json"
               try:
-                size = os.path.getsize(profile_file)
-                self.user = open(profile_file)
-                self.user = json.load(self.user)              
+                size = os.path.getsize(profile_file)      # Get size of profile
+                self.user = json.load(open(profile_file)) # Read in user profile
                 send_message = self.clientcmd.profile_message(message[1], message[2], json.dumps(self.user))
               except: 
                 size = 0
                 send_message = "I don't have the requested profile."
-              message_queues[s].put(pickle.dumps(size))
-              message_queues[s].put(pickle.dumps(send_message))
+              message_queues[s].put(pickle.dumps(size))         # Send size
+              message_queues[s].put(pickle.dumps(send_message)) # Send profile
 
             elif message[0] == "GET": 
               try: 
@@ -286,20 +282,19 @@ class Client:
               except:
                 size = 0
                 send_message = "File does not exist"
-              message_queues[s].put(pickle.dumps(size))
-              message_queues[s].put(pickle.dumps(send_message))
+              message_queues[s].put(pickle.dumps(size))         # Send size
+              message_queues[s].put(pickle.dumps(send_message)) # Send requested file
 
             elif message[0] == "PING":
-              send_message = self.clientcmd.pong_server(self.username, host, port)
-              message_queues[s].put(pickle.dumps(send_message))            
+              send_message = self.clientcmd.pong_server(self.username, self.host, self.port)
+              message_queues[s].put(pickle.dumps(send_message)) # Send pong back to server
 
             else:
               print("Command " + str(message[0]) + " not recognized")
               message_queues[s].put(data)
 
-            # Add output channel for response
             if s not in outputs:
-              outputs.append(s)
+              outputs.append(s) # Add output channel for response
 
           else: # Interpret empty result as closed connection
             print("Peer dropped out, closing connection")
@@ -318,31 +313,21 @@ class Client:
           next_msg = message_queues[s].get_nowait()
         except queue.Empty:
           # No messages waiting so stop checking for writability.
-          print(sys.stderr)
-          print('output queue for')
-          print(s.getpeername())
-          print('is empty')
           outputs.remove(s)
         else:
-          print(sys.stderr)
-          print('sending "%s" to %s' % (next_msg, s.getpeername()))
           s.send(next_msg)
 
       # Handle "exceptional conditions"
       for s in exceptional:
-        print ('handling exceptional condition for')
-        print(s.getpeername())
         # Stop listening for input on the connection
         inputs.remove(s)
         if s in outputs:
           outputs.remove(s)
         s.close()
 
-        # Remove message queue
-        del message_queues[s]
-  
-    # Close tcp connection when server exits
-    tcp_socket.close()
+        del message_queues[s] # Remove message queue
+ 
+    tcp_socket.close() # Close tcp connection when server exits
 
 if __name__ == '__main__':
   client = Client(sys.argv[1], sys.argv[2], sys.argv[3])
